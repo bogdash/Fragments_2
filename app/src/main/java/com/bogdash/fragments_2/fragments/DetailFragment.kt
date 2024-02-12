05a8bcb51b3ea60cbba8668376c1bc65d338269d
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-02-12 17:43:01 +0300

    Add image picker

--- app/src/main/java/com/bogdash/fragments_2/fragments/DetailFragment.kt
+++ app/src/main/java/com/bogdash/fragments_2/fragments/DetailFragment.kt
@@ -1,10 +1,17 @@
 package com.bogdash.fragments_2.fragments
 
+import android.app.Activity
+import android.app.Instrumentation.ActivityResult
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.setFragmentResult
 import com.bogdash.fragments_2.R
 import com.bogdash.fragments_2.UserData
@@ -32,6 +39,12 @@ class DetailFragment(
             edPhone.setText(user.phone)
             ivPhotoUser.setImageResource(user.photo)
 
+            ivPhotoUser.setOnClickListener{
+                Toast.makeText(requireContext(), "hi", Toast.LENGTH_SHORT).show()
+                val pickImageIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                pickImage.launch(pickImageIntent)
+            }
+
             btnEdit.setOnClickListener {
                 user.firstName = tiedFirstname.text.toString()
                 user.lastName = tiedLastname.text.toString()
@@ -45,6 +58,14 @@ class DetailFragment(
             btnCancel.setOnClickListener {
                 requireActivity().supportFragmentManager.popBackStack()
             }
+        }
+    }
+
+    private val pickImage = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            val selectedImageUri = data?.data
+            binding.ivPhotoUser.setImageURI(selectedImageUri)
         }
     }
 
