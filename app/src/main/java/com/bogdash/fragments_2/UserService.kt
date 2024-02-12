f577248d53bfc4c754fa4b82f02ee031d3455b48
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-02-12 16:32:34 +0300

    Update users

--- app/src/main/java/com/bogdash/fragments_2/UserService.kt
+++ app/src/main/java/com/bogdash/fragments_2/UserService.kt
@@ -5,9 +5,13 @@ class UserService {
         fun getUsers(): ArrayList<UserData> {
             return arrayListOf(
                 UserData(0, R.drawable.photo_1, "Ivan", "Ivanov", "8 965 786 54 56"),
-                UserData(0, R.drawable.photo_2, "Petya", "Petrov", "8 865 566 54 56"),
-                UserData(0, R.drawable.photo_3, "Aleksey", "Alekseev", "8 965 456 54 32"),
-                UserData(0, R.drawable.photo_4, "Mark", "Markov", "8 123 786 54 32")
+                UserData(1, R.drawable.photo_2, "Petya", "Petrov", "8 865 566 54 56"),
+                UserData(2, R.drawable.photo_3, "Aleksey", "Alekseev", "8 965 456 54 32"),
+                UserData(3, R.drawable.photo_4, "Mark", "Markov", "8 123 786 54 32"),
+                UserData(4, R.drawable.photo_1, "Ivan", "Ivanov", "8 965 786 54 56"),
+                UserData(5, R.drawable.photo_2, "Petya", "Petrov", "8 865 566 54 56"),
+                UserData(6, R.drawable.photo_3, "Aleksey", "Alekseev", "8 965 456 54 32"),
+                UserData(7, R.drawable.photo_4, "Mark", "Markov", "8 123 786 54 32")
             )
         }
     }
